873c7c2a62c4acf6e476340ee2d86ba628780d37
kolheyash226-dev <kolheyash226@gmail.com>
2026-02-12 11:41:51 +0530

    Lab1

---
+++ Lab1/Lab1Que2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Enter the first number:");
+     
+        int n1 = Convert.ToInt32(Console.ReadLine());
+
+        
+        Console.WriteLine("Enter the second number:");
+        
+        int n2 = Convert.ToInt32(Console.ReadLine());
+       
+        int result = n1 + n2;
+
+      
+        Console.WriteLine("The sum is: " + result);
+    }
+}
