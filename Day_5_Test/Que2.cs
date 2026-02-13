a7b517929c5e613ddf1a9f203ebc9038bf03c6b4
kolheyash226-dev <kolheyash226@gmail.com>
2026-02-13 14:30:04 +0530

    test

---
+++ Day_5_Test/Que2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questions
+{
+    internal class Question2
+    {
+        static bool IsPalindrome(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in s)
+            {
+                if (char.IsLetterOrDigit(c))
+                {
+                    sb.Append(char.ToLower(c));
+                }
+            }
+            string str = sb.ToString();
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            string reverse = new string(arr);
+            if (str != reverse)
+            {
+                return false;
+            }
+            return true;
+        }
+        static void Main()
+        {
+            Console.WriteLine("Enter The String :");
+            string s = Console.ReadLine();
+            Console.WriteLine(IsPalindrome(s));
+        }
+    }
+}
